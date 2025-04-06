272318e9d849812126b3a3fd222cb5b6222b91ca
Krishna <singhkrishna9689@gmail.com>
2025-04-06 21:00:57 +0530

    sub

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Child from "./Child";
 
 const App = () => {
+  let [isLoggedIn, setisLoggedIn] = useState(false)
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Parent Component </h1>
+      <Child
+      isLoggedIn = {isLoggedIn}
+      setisLoggedIn = {setisLoggedIn}
+       />
+    
     </div>
   )
 }
---
+++ src/components/Child.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const Child = ({isLoggedIn, setisLoggedIn}) => {
+  return (
+    <div className='child'>
+    <form onSubmit={(e)=> {
+        e.preventDefault()
+        setisLoggedIn(true)
+    }}>
+      <label>Username:</label>
+      <input type="text" /> 
+      <label>Password:</label>
+      <input type="password" />        
+      <button>Login</button>
+    </form>
+    {
+        isLoggedIn==true && <p>You are logged in!</p>
+    }
+    </div>
+  )
+}
+
+export default Child
+
+// import React from 'react'
+
+// const Child = ({ isLoggedIn, setisLoggedIn }) => {
+//   return (
+//     <div className='child'>
+//       {
+//         isLoggedIn ? (
+//           <p>You are logged in!</p>
+//         ) : (
+//           <form onSubmit={(e) => {
+//             e.preventDefault()
+//             setisLoggedIn(true)
+//           }}>
+//             <label>Username:</label>
+//             <input type="text" />
+//             <label>Password:</label>
+//             <input type="password" />
+//             <button>Login</button>
+//           </form>
+//         )
+//       }
+//     </div>
+//   )
+// }
+
+// export default Child
